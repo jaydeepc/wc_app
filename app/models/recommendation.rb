55849c5ae6ab21cc561a67181ad3e36f3dfef337
Alejandro Beiderman <alejandrobeiderman@abeiderman.local>
2014-10-16 14:38:59 -0400

    --amend

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,18 +1,20 @@
 class Recommendation
   include ActiveModel::Model
 
-  attr_accessor :text
+  attr_accessor :text, :price, :img, :link
+  @landing_page_url = 'http://localhost:3000/'
+  @img_url = 'http://placehold.it/350x150'
 
   def self.find_by_answers answers
     recommendations = {
-      ['Small', 'Few', 'Traditional'] => Recommendation.new(text: 'American Gourmet'),
-      ['Small', 'Lots', 'Traditional'] => Recommendation.new(text: 'Classic 2 burner gas'),
-      ['Small', 'Few', 'New'] => Recommendation.new(text: 'Patio bistro electric'),
-      ['Small', 'Lots', 'New'] => Recommendation.new(text: 'Commercial 2 burner'),
-      ['Big', 'Few', 'Traditional'] => Recommendation.new(text: 'Classic 3 burner gas'),
-      ['Big', 'Lots', 'Traditional'] => Recommendation.new(text: 'Classic 6 burner'),
-      ['Big', 'Few', 'New'] => Recommendation.new(text: 'Performance 3 burner'),
-      ['Big', 'Lots', 'New'] => Recommendation.new(text: 'Performance 4 burner')
+      ['Small', 'Few', 'Traditional'] => Recommendation.new(text: 'American Gourmet', price: 129.99, link: @landing_page_url, img: @img_url),
+      ['Small', 'Lots', 'Traditional'] => Recommendation.new(text: '2 burner gas', price: 189, link: @landing_page_url, img: @img_url),
+      ['Small', 'Few', 'New'] => Recommendation.new(text: 'Patio bistro electric', price: 139, link: @landing_page_url, img: @img_url),
+      ['Small', 'Lots', 'New'] => Recommendation.new(text: 'Commercial 2 burner', price: 269, link: @landing_page_url, img: @img_url),
+      ['Big', 'Few', 'Traditional'] => Recommendation.new(text: 'Classic 3 burner gas', price: 199.99, link: @landing_page_url, img: @img_url),
+      ['Big', 'Lots', 'Traditional'] => Recommendation.new(text: 'Classic 6 burner', price: 389, link: @landing_page_url, img: @img_url),
+      ['Big', 'Few', 'New'] => Recommendation.new(text: 'Performance 3 burner', price: 349, link: @landing_page_url, img: @img_url),
+      ['Big', 'Lots', 'New'] => Recommendation.new(text: 'Performance 4 burner', price: 549, link: @landing_page_url, img: @img_url)
     }
 
     recommendations[answers]
--- spec/models/recommendation_spec.rb
+++ spec/models/recommendation_spec.rb
@@ -2,19 +2,19 @@ require "rails_helper"
 
 describe Recommendation do
   describe '.find_by_answers' do
-    def self.it_returns_the_grill_for answers, grill_name
-      it "returns #{grill_name} for #{answers.join(',')}" do
-        expect(Recommendation.find_by_answers(answers).text).to eql grill_name
+    def self.it_returns_the_grill_for answers, grill_info
+      it "returns #{grill_info} for #{answers.join(',')}" do
+        expect(Recommendation.find_by_answers(answers).text).to eql grill_info[:name]
       end
     end
 
-    it_returns_the_grill_for ['Small', 'Few', 'Traditional'], 'American Gourmet'
-    it_returns_the_grill_for ['Small', 'Lots', 'Traditional'], 'Classic 2 burner gas'
-    it_returns_the_grill_for ['Small', 'Few', 'New'], 'Patio bistro electric'
-    it_returns_the_grill_for ['Small', 'Lots', 'New'], 'Commercial 2 burner'
-    it_returns_the_grill_for ['Big', 'Few', 'Traditional'], 'Classic 3 burner gas'
-    it_returns_the_grill_for ['Big', 'Lots', 'Traditional'], 'Classic 6 burner'
-    it_returns_the_grill_for ['Big', 'Few', 'New'], 'Performance 3 burner'
-    it_returns_the_grill_for ['Big', 'Lots', 'New'], 'Performance 4 burner'
+    it_returns_the_grill_for ['Small', 'Few', 'Traditional'], { name: 'American Gourmet' }
+    it_returns_the_grill_for ['Small', 'Lots', 'Traditional'], { name: '2 burner gas' }
+    it_returns_the_grill_for ['Small', 'Few', 'New'], { name: 'Patio bistro electric' }
+    it_returns_the_grill_for ['Small', 'Lots', 'New'], { name: 'Commercial 2 burner' }
+    it_returns_the_grill_for ['Big', 'Few', 'Traditional'], { name: 'Classic 3 burner gas' }
+    it_returns_the_grill_for ['Big', 'Lots', 'Traditional'], { name: 'Classic 6 burner' }
+    it_returns_the_grill_for ['Big', 'Few', 'New'], { name: 'Performance 3 burner' }
+    it_returns_the_grill_for ['Big', 'Lots', 'New'], { name: 'Performance 4 burner' }
   end
 end
